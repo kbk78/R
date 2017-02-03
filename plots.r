9b048a9ba31e998a0a47a69b0691c874384cfee5
kbk78 <planetkbk@gmail.com>
2017-02-03 16:08:20 -0600

    init

---
+++ plots.r
@@ -0,0 +1,6 @@
+EUI=c(14.9,23,3.7,7.1,9.7,1.3)
+cat=c('Heating','Cooling','Lighting','Equipment','Fans','Pumps')
+val=data.frame(cat,EUI)
+ggplot(val,aes(cat,EUI))+geom_col()+xlab('')+ylab('EUI [kBtu/sqft]')
+
+
