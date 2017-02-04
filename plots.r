fc7647e5616e2abbb75adfbb5c13ba9e80faf7a6
kbk78 <planetkbk@gmail.com>
2017-02-03 19:54:58 -0600

    mod1

--- plots.r
+++ plots.r
@@ -2,3 +2,5 @@ EUI=c(14.9,23,3.7,7.1,9.7,1.3)
 cat=c('Heating','Cooling','Lighting','Equipment','Fans','Pumps')
 val=data.frame(cat,EUI)
 ggplot(val,aes(cat,EUI))+geom_col()+xlab('')+ylab('EUI [kBtu/sqft]')
+
+test=c(1,2,3)
